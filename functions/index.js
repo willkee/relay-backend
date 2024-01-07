3e6586ad916bd1256cfc685c2340c06d82d60399
Will Kee <william.w.kee@gmail.com>
2024-01-06 19:49:16 -0700

    user saved to authenticated users, have trouble saving to db collection -- work in progress

--- functions/index.js
+++ functions/index.js
@@ -12,6 +12,10 @@ const firebaseConfig = {
 	appId: process.env.FB_APP_ID,
 	measurementId: process.env.FB_MEASUREMENT_ID,
 };
+const functions = require("firebase-functions");
+
+const admin = require("firebase-admin");
+// admin.initializeApp(functions.config().firebase);
 
 initializeApp(firebaseConfig);
 
@@ -22,20 +26,60 @@ const app = express();
 
 const { getAuth, createUserWithEmailAndPassword } = require("firebase/auth");
 
-const defaultFirestore = getFirestore();
+const db = getFirestore();
 const auth = getAuth();
 
 app.use(express.json());
-app.use(cors({ origin: true }));
+app.use(
+	cors({
+		origin: "https://www.google.com",
+	})
+);
+
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/hello", (req, res) => {
+app.get("/stuff", async (req, res) => {
+	console.log(req.headers.referer, "referrer");
 	res.send("Testing hello world Relay backend!");
 });
 
-// app.post("/register", async (req, res) => {
-//     const { email, password } = req.body;
+app.post("/register", async (req, res) => {
+	const { email, password } = req.body;
 
-// });
+	// TODO: SET UP PASSWORD COMPLEXITY CHECK
+
+	try {
+		const response = await createUserWithEmailAndPassword(
+			auth,
+			email,
+			password
+		);
+
+		// console.log(
+		// 	"\n\n\n\n\n LOGGER \n\n\n\n",
+		// 	response.user.uid,
+		// 	"\n\n\n\n\n LOGGER \n\n\n\n"
+		// );
+
+		if (response?.user?.uid) {
+			const uid = response.user.uid;
+
+			// console.log(db, "db?");
+
+			const dbResponse = await db.collection("users").doc(uid).set({
+				email,
+			});
+
+			// console.log(dbResponse, "db response");
+
+			res.send(`Success. UID: ${response.uid}`);
+		}
+	} catch (err) {
+		console.log("Error: ", err);
+		res.status(500).json({
+			error: err.message,
+		});
+	}
+});
 
 exports.app = onRequest(app);
