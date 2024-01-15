6e483cd5ccabd9613cbde0dae1aabeb419054da0
Will Kee <william.w.kee@gmail.com>
2024-01-15 14:45:58 -0700

    commented out unused routes

--- functions/routes/auth.js
+++ functions/routes/auth.js
@@ -1,39 +1,12 @@
-// const axios = require("axios");
 const express = require("express");
+const router = express.Router();
+
 const asyncHandler = require("express-async-handler");
 const { getAuth } = require("firebase-admin/auth");
 const { getFirestore } = require("firebase-admin/firestore");
-const router = express.Router();
 
 const auth = getAuth();
 const db = getFirestore();
-
-// const FIREBASE_API_KEY = process.env.FB_WEB_API_KEY;
-
-router.get("/checkCookies", (req, res) => {
-	res.json({ sessionCookie: !!req.cookies.session });
-});
-
-router.post(
-	"/sessionLogin",
-	asyncHandler(async (req, res) => {
-		const { idToken, xsrfToken } = req.body;
-		const id = idToken.toString();
-		const xsrf = xsrfToken.toString();
-
-		if (xsrf !== req.cookies["XSRF-TOKEN"]) {
-			return res.status(403).send("Unauthorized request!");
-		}
-
-		// Set session expiration to 5 days.
-		const expiresIn = 60 * 60 * 24 * 5 * 1000;
-
-		const sessionCookie = await auth.createSessionCookie(id, { expiresIn });
-		const options = { maxAge: expiresIn, httpOnly: true, secure: true };
-		res.cookie("session", sessionCookie, options);
-		res.json({ success: true });
-	})
-);
 
 router.post(
 	"/register",
@@ -54,42 +27,29 @@ router.post(
 	})
 );
 
+// router.get("/checkCookies", (req, res) => {
+// 	res.json({ sessionCookie: !!req.cookies.session });
+// });
+
 // router.post(
-// 	"/login",
+// 	"/sessionLogin",
 // 	asyncHandler(async (req, res) => {
-// 		const { email, password } = req.body;
+// 		const { idToken, xsrfToken } = req.body;
+// 		const id = idToken.toString();
+// 		const xsrf = xsrfToken.toString();
 
-// 		const user = await auth.getUserByEmail(email);
-
-// 		if (!user || user.disabled)
-// 			throw new Error("User does not exist or has been disabled.");
-
-// 		const { data } = await axios.post(
-// 			`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${FIREBASE_API_KEY}`,
-// 			{ email, password, returnSecureToken: true }
-// 		);
-
-// 		const id = data?.idToken;
+// 		if (xsrf !== req.cookies["XSRF-TOKEN"]) {
+// 			return res.status(403).send("Unauthorized request!");
+// 		}
 
 // 		// Set session expiration to 5 days.
 // 		const expiresIn = 60 * 60 * 24 * 5 * 1000;
 
 // 		const sessionCookie = await auth.createSessionCookie(id, { expiresIn });
 // 		const options = { maxAge: expiresIn, httpOnly: true, secure: true };
-
-// 		res.cookie("id_token", id, options);
 // 		res.cookie("session", sessionCookie, options);
-// 		res.json({
-// 			email: user.email,
-// 			uid: user.uid,
-// 			displayName: user.displayName,
-// 			phoneNumber: user.phoneNumber,
-// 		});
+// 		res.json({ success: true });
 // 	})
 // );
-
-// router.get("/reload", async (req, res) => {
-// 	console.log(req.cookies, "cookies");
-// });
 
 module.exports = router;
